5dcecdb27656e10e6bfc5ef6f5ced7bd11f2818b
Christina Meizenitsch <s53093@edu.campus02.at>
2024-02-07 20:49:23 +0100

    Änderung sout Main und Anlage Testklasse

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -1,7 +1,10 @@
+package FirstProject.src;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Christina Meizenitsch");
         System.out.println("Schoki");
         System.out.println("Donnerstag");
+        System.out.println("ERGÄNZUNG");
     }
 }
