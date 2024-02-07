7f0d5040acccdcf052a7e2666c16c6d0c5ec8733
fhc02cm <145900535+cmeizenitsch@users.noreply.github.com>
2024-02-07 20:47:53 +0100

    Add files via upload

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Christina Meizenitsch");
+        System.out.println("Schoki");
+        System.out.println("Donnerstag");
+    }
+}
